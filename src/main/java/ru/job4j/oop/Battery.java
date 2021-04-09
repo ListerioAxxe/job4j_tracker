69725c4bf74a079be08cc0bf848718406cd30739
ListerioAxxe <langua095@gmail.com>
2021-04-09 19:30:45 +0300

    Battery

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load; // создали поле заряд батареи
+
+    public Battery(int accum) {  // конструктор Батарея входящий параметр заряда
+        this.load = accum;        // обратились к полю , передали параметр заряда в поле
+    }
+
+    public void exchange(Battery another) {   // метод обмена зарядом
+        this.load = this.load + another.load;  // в поле сумму зарядов обоих батарей (обращаемся к полю
+        another.load = 0;  // вторая батарея пуста
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(15);
+        Battery second = new Battery(10);
+        first.exchange(second);
+        System.out.println("first " + first.load + " second " + second.load );
+    }
+}
