301382b5442c15547de4f12233f1ce2226099447
ListerioAxxe <langua095@gmail.com>
2021-04-16 18:50:31 +0300

    Item

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,24 +1,30 @@
 package ru.job4j.tracker;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
+    LocalDateTime currentDateTime = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {return getCreated(); }
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
+
 
     public Item() {
     }
@@ -30,8 +36,12 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
-
     }
 
+    public class StartUI {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTime = item.getCreated().format(formatter);
+    }
 
 }
