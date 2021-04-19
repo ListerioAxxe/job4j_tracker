4e83c19dda699d9d4706890eab80df2743658da1
ListerioAxxe <langua095@gmail.com>
2021-04-19 14:00:17 +0300

    tooString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,10 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-      Tracker tracker = new Tracker();
       Item item = new Item();
-      tracker.add(item);
-      Item rsl = tracker.findById(5);
-        System.out.println(rsl);
+        System.out.println(item);
     }
 }
