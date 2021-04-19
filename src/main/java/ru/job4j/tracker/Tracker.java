928cdd1c8ff4a918985e843c63b35aec22971876
ListerioAxxe <langua095@gmail.com>
2021-04-19 13:46:15 +0300

    TrackerValidation

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -46,39 +46,25 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-
         int idIndex = indexOf(id);
-
         if (idIndex != -1) {
-
             item.setId(id);
-
             items[idIndex] = item;
-
             return true;
-
         }
-
         return false;
 
     }
 
     public boolean delete(int id) {
        int itemDelete = indexOf(id);
-
             if (items[itemDelete] != null ) {
-
                 System.arraycopy(items, itemDelete+1, items, itemDelete, size-itemDelete - 1);
                 items[size-1] = null;
-
                 size--;
-
                 return true;
-
             }
-
         return false;
-
     }
 
 
