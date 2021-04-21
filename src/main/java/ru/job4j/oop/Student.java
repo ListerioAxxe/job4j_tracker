58031ce1bf6bfffc615f4acbb6a049755db1177e
ListerioAxxe <langua095@gmail.com>
2021-04-21 18:15:06 +0300

    Student

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,24 +1,5 @@
 package ru.job4j.oop;
 
-import java.sql.SQLOutput;
-
 public class Student {
 
-    public void music() {
-        System.out.println("тра та та");
-    }
-    public void song(){
-        System.out.println("I believe I can fly");
-    }
-
-    public static void main(String[] args) {
-        Student petya = new Student();
-        petya.music();
-        petya.song();
-        petya.music();
-        petya.song();
-        petya.music();
-        petya.song();
-
-    }
 }
