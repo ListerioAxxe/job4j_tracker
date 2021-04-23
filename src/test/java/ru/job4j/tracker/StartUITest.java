521ec34eacf0da1dc90e53648551d11bbc216588
ListerioAxxe <langua095@gmail.com>
2021-04-23 17:54:12 +0300

    itemDeleteTest

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,8 +1,10 @@
 package ru.job4j.tracker;
 
 import junit.framework.TestCase;
+import org.hamcrest.Matcher;
+import org.junit.Assert;
+import org.junit.Test;
 
-import java.net.Inet4Address;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -17,6 +19,32 @@ public class StartUITest extends TestCase {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "replaced item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        Assert.assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    public void testDelete() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("first item");
+        tracker.add(item);
+        String[] answers ={String.valueOf(item.getId()), "deleted item"};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        Item exp = null;
+        Assert.assertThat(deleted, is(exp));
 
     }
 }
