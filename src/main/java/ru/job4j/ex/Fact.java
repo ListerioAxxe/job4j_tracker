6e9a0fea01488085c1c30efbaa02e8ff48406421
ListerioAxxe <langua095@gmail.com>
2021-04-26 19:29:31 +0300

    FActExcep

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,16 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(-3));
+    }
+
+    public int calc(int n) {
+        if (n < 0) { throw new IllegalArgumentException("Argument lower than 0"); }
+            int rsl = 1;
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+            return rsl;
+    }
+}
