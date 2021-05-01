257d912ea79655f5041d782a09bddc4a70e1369b
ListerioAxxe <langua095@gmail.com>
2021-05-01 13:22:57 +0300

    ValidateTest

--- src/test/java/ru/job4j/tracker/ValidateTest.java
+++ src/test/java/ru/job4j/tracker/ValidateTest.java
@@ -11,10 +11,38 @@ public class ValidateTest {
     public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"-1", "1"}
+                new String[] {"0", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
         assertThat(selected, is(1));
     }
+
+    @Test
+    public void whenInvalidInput1() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(2));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(3));
+    }
+
+    @Test
+    public void whenInvalidInputError() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1", "-2", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+
 }
