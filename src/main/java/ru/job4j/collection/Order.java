f92859f3db0050f7320049f15b05197db4d19c9c
ListerioAxxe <langua095@gmail.com>
2021-05-05 20:10:43 +0300

    OrdersSecondTest

--- src/main/java/ru/job4j/collection/Order.java
+++ src/main/java/ru/job4j/collection/Order.java
@@ -23,10 +23,15 @@ public class Order {
                 '}';
     }
 
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+           return false;
+        }
         Order order = (Order) o;
         return Objects.equals(number, order.number) &&
                 Objects.equals(name, order.name);
