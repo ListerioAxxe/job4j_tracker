452890924b5613d085b667b05242c89abfed1176
ListerioAxxe <langua095@gmail.com>
2021-05-07 19:23:47 +0300

    Банк

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,53 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        List<Account> accounts = users.get(user);
+        if (!accounts.contains(account)) {
+            accounts.add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+      for (User user : users.keySet()) {
+          if (user.getPassport().contains(passport)) {
+              return user;
+          }
+      }
+      return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        for (Account account : users.get(user)) {
+            if (account.getRequisite().equals(requisite) && user != null) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+       Account scrAcc = findByRequisite(srcPassport, srcRequisite);
+       Account destAcc = findByRequisite(destPassport, destRequisite);
+       if (scrAcc != null && destAcc != null && scrAcc.getBalance() >= amount) {
+           scrAcc.setBalance(scrAcc.getBalance() - amount);
+           destAcc.setBalance(destAcc.getBalance() + amount);
+           return true;
+       }
+        return false;
+    }
+}
