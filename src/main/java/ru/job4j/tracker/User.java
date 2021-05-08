80aa2a3b80dcf9d29626e140d685ec6effbe5999
ListerioAxxe <langua095@gmail.com>
2021-05-08 20:58:04 +0300

    СортировкаUser

--- src/main/java/ru/job4j/tracker/User.java
+++ src/main/java/ru/job4j/tracker/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
-public class User {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class User implements Comparable<User>{
     private int id;
 
     private String username;
@@ -22,6 +25,11 @@ public class User {
         this.password = password;
     }
 
+    public User(String username, int id) {
+        this.id = id;
+        this.username = username;
+    }
+
     public int getId() {
         return id;
     }
@@ -32,5 +40,28 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
+
+    @Override
+    public int compareTo(User o) {
+       if (this.getUsername().equals(o.getUsername())) {
+           return this.getId() - o.getId();
+       } else {
+           return this.getUsername().compareTo(o.getUsername());
+       }
+
     }
 }
