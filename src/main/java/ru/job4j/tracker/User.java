0524cf4860049d4ab48b13aca1635061fb7423ba
ListerioAxxe <langua095@gmail.com>
2021-05-09 14:05:53 +0300

    Переделка компараций

--- src/main/java/ru/job4j/tracker/User.java
+++ src/main/java/ru/job4j/tracker/User.java
@@ -5,10 +5,9 @@ import java.util.Objects;
 
 public class User implements Comparable<User> {
     private int id;
-
     private String username;
-
     private String password;
+    private int age;
 
     @Override
     public String toString() {
@@ -34,35 +33,59 @@ public class User implements Comparable<User> {
         return id;
     }
 
-    public String getUsername() {
-        return username;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
-        return id == user.id && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+        return id == user.id && age == user.age && Objects.equals(username, user.username) && Objects.equals(password, user.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, password);
+        return Objects.hash(id, username, password, age);
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @Override
     public int compareTo(User o) {
        int rsl =  this.getUsername().compareTo(o.getUsername());
        if (rsl == 0) {
-           return Integer.compare(this.getId(), o.getId());
+           return Integer.compare(this.getAge(), o.getAge());
        }
        return rsl;
 
-      // или так return  Comparator.comparing(User::getUsername).thenComparing(User::getId).compare(this, o);
+      // или так return  Comparator.comparing(User::getUsername).thenComparing(User::getAge).compare(this, o);
     }
 }
