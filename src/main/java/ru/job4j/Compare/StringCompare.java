11e8c5e706eb09af268ec69ac3cc89ca4aa7eb02
ListerioAxxe <langua095@gmail.com>
2021-05-09 16:38:01 +0300

    КомпараторСтринг

---
+++ src/main/java/ru/job4j/Compare/StringCompare.java
@@ -0,0 +1,16 @@
+package ru.job4j.Compare;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Integer.compare(left.charAt(i), right.charAt(i));
+            }
+        }
+        return Integer.compare(left.length(), right.length());
+    }
+}
