6b731abf2a5ecea77207c89886dae2aa2a65a989
ListerioAxxe <langua095@gmail.com>
2021-05-09 11:39:29 +0300

    Сортировка

--- src/main/java/ru/job4j/Compare/CompareIdRevers.java
+++ src/main/java/ru/job4j/Compare/CompareIdRevers.java
@@ -1,14 +1,13 @@
 package ru.job4j.Compare;
 
+
 import ru.job4j.tracker.Item;
 
 import java.util.Comparator;
 
-public class CompareToName implements Comparator<Item> {
-
+public class CompareIdRevers implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
-        return o1.getName().compareTo(o2.getName());
+        return Integer.compare(o2.getId(), o1.getId());
     }
 }
-
