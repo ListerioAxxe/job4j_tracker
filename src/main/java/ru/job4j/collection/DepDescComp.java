03dbf2b149eda775428e6b7c4c94135c20e91fd1
ListerioAxxe <langua095@gmail.com>
2021-05-10 19:58:10 +0300

    корректировка

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,9 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] start = o1.split("/");
         String[] end = o2.split("/");
         int x = end[0].compareTo(start[0]);
-        if (x == 0) {
-            return o1.compareTo(o2);
-        }
-        return x;
+        return  x == 0 ? o1.compareTo(o2) : x;
     }
 }
