71497232eebd283c101c178f87e5d87a4c7e61d3
ListerioAxxe <langua095@gmail.com>
2021-05-14 20:28:45 +0300

    LambdaUsage

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.function;
+
+import java.util.Comparator;
+
+@SuppressWarnings("checkstyle:EmptyLineSeparator")
+public class LambdaUsage {
+    public static void main(String[] args) {
+
+        Comparator<String> cmpText1 = (left, right) -> {
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return left.length() - right.length();
+        };
+
+        Comparator<String> cmpDescSize1 = (left, right) -> {
+           System.out.println("compare - " + left.length() + " : " + right.length());
+           return right.length() - left.length();
+        };
+    }
+}
