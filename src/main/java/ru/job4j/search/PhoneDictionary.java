b7f06341939b28ac5909d2f0d308c6a40532deb3
ListerioAxxe <langua095@gmail.com>
2021-05-15 18:53:35 +0300

    Dictionary

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -16,13 +18,28 @@ public class PhoneDictionary {
      */
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> byPhone = person -> person.getPhone().contains(key);
+        Predicate<Person> byName = person -> person.getName().contains(key);
+        Predicate<Person> bySurname = person -> person.getSurname().contains(key);
+        Predicate<Person> byAdress = person -> person.getAddress().contains(key);
+        Predicate<Person> combine = byAdress.or(byName).or(byPhone).or(bySurname);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key)
-               || person.getAddress().contains(key) || person.getPhone().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
         return result;
     }
 }
+
+//   public ArrayList<Person> find(String key) {
+//       ArrayList<Person> result = new ArrayList<>();
+//       for (Person person : persons) {
+//          if (person.getName().contains(key) || person.getSurname().contains(key)
+//             || person.getAddress().contains(key) || person.getPhone().contains(key)) {
+//              result.add(person);
+//          }
+//       }
+//      return result;
+//   }
