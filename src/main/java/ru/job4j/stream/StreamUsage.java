fa86122adf1ffb4f5f2ee02211fc9de8b71de5c5
ListerioAxxe <langua095@gmail.com>
2021-05-16 14:05:54 +0300

    Stream Usage

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class StreamUsage {
     public static void main(String[] args) {
         List<Integer> list = List.of(-1, 1, 5, 7, -3);
-        List<Integer> result =  list.stream().filter(x -> x >= 0).collect(Collectors.toList());
+        List<Integer> result =  list.stream().filter(x -> x > 0).collect(Collectors.toList());
         System.out.println(result.toString());
     }
 }
