e61e80cdd1a9e96fa33e13a1879f50469484c949
ListerioAxxe <langua095@gmail.com>
2021-05-16 17:44:25 +0300

    Adress Correct

--- src/main/java/ru/job4j/stream/profiles/Address.java
+++ src/main/java/ru/job4j/stream/profiles/Address.java
@@ -4,24 +4,9 @@ import java.util.Objects;
 
 public class Address {
     private String city;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Address address = (Address) o;
-        return home == address.home && apartment == address.apartment
-                && Objects.equals(city, address.city) && Objects.equals(street, address.street);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(city, street, home, apartment);
-    }
+    private String street;
+    private int home;
+    private int apartment;
 
     public Address(String city, String street, int home, int apartment) {
         this.city = city;
@@ -46,9 +31,30 @@ public class Address {
         return apartment;
     }
 
-    private String street;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return home == address.home && apartment == address.apartment && Objects.equals(city, address.city) && Objects.equals(street, address.street);
+    }
 
-    private int home;
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
 
-    private int apartment;
+    @Override
+    public String toString() {
+        return "Address{"
+                + "city='" + city + '\''
+                + ", street='" + street + '\''
+                + ", home=" + home
+                + ", apartment=" + apartment
+                + '}';
+    }
 }
