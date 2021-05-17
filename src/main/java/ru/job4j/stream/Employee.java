b5b561d81f516b97a2910a9d64714ec27c60fdd4
ListerioAxxe <langua095@gmail.com>
2021-05-17 18:00:20 +0300

    Builder

---
+++ src/main/java/ru/job4j/stream/Employee.java
@@ -0,0 +1,61 @@
+package ru.job4j.stream;
+
+public class Employee {
+    private String specialization;
+    private String grade;
+    private int cabinet;
+    private char gender;
+    private String education;
+    private boolean assist;
+
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
+    static class Builder {
+        private String specialization;
+        private String grade;
+        private int cabinet;
+        private char gender;
+        private String education;
+        private boolean assist;
+
+        Builder buildSpecialization(String specialization) {
+            this.specialization = specialization;
+            return this;
+        }
+
+        Builder buildGrade(String grade) {
+            this.grade = grade;
+            return this;
+        }
+
+        Builder buildCabinet(int cabinet) {
+            this.cabinet = cabinet;
+            return this;
+        }
+
+        Builder buildGender(char gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        Builder buildEducation(String education) {
+            this.education = education;
+            return this;
+        }
+
+        Builder buildAssist(boolean assist) {
+            this.assist = assist;
+            return this;
+        }
+
+        Employee build() {
+                Employee employee = new Employee();
+            employee.specialization = specialization;
+            employee.grade = grade;
+            employee.cabinet = cabinet;
+            employee.gender = gender;
+            employee.education = education;
+            employee.assist = assist;
+                return employee;
+            }
+        }
+    }
